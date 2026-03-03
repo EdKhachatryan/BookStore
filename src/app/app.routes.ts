3796a86a596f8731466f44f2468a25ae905f275f
EdKhachatryan <eduard.khachatrian@gmail.com>
2026-03-03 03:53:46 +0100

    bugfix(overview): routing module fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
-import { BooksListComponent } from '@app/modules/books/pages/books-list/books-list.component';
-import { OverviewComponent } from '@app/modules/books/pages/overview/overview.component';
 import { booksInitResolver } from '@core/resolvers/ books-init.resolver';
 
 export const routes: Routes = [
-  { path: '', component: OverviewComponent, resolve: { booksReady: booksInitResolver } },
-  { path: 'list', component: BooksListComponent, resolve: { booksReady: booksInitResolver } },
+  {
+    path: '',
+    resolve: { booksReady: booksInitResolver },
+    loadComponent: () => import('@app/modules/books/pages/overview/overview.component').then(m => m.OverviewComponent),
+  },
+  {
+    path: 'list',
+    resolve: { booksReady: booksInitResolver },
+    loadComponent: () =>
+      import('@app/modules/books/pages/books-list/books-list.component').then(m => m.BooksListComponent),
+  },
 ];
